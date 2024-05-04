6d8524204b43a0d2b6354107007370981f888b81
boonff <qxxpdada@163.com>
2024-05-04 17:19:02 +0800

    v1.1

--- src/main.py
+++ src/main.py
@@ -1,16 +1,11 @@
 from noval import Noval
 
 if __name__ == "__main__":
-    chapter_sign = r"^\s*(?:[上下]+[章回部节集卷话]+\s*)?[第卷话](?:\d+|[一二三四五六七八九十零〇百千两]+)[章回部节集卷话](.*)"
-    part_sign = r"(^\s*\d{3}.*)"
+
+    novel = Noval(in_path="docs/in/物语epub", out_path="docs/out/")
+
     roll_position = {
-        "怪人的沙拉碗3.txt": "第四卷",
+        "[物语系列][05卷]伪物语（下）.epub": "伪物语下",
     }
 
-    novel = Noval(
-        chapter_sign,
-        part_sign,
-        in_path="docs/in/怪人的沙拉碗",
-        out_path="docs/out/怪人的沙拉碗",
-    )
     novel.run(roll_position)
--- src/noval.py
+++ src/noval.py
@@ -1,34 +1,68 @@
 import os
 
+import ebooklib
+from ebooklib import epub
 from EdgeTTS import edge_tts_string
 from cut import chapter_cut, part_cut
+from bs4 import BeautifulSoup
+import unicodedata
 
 
 class Noval:
-    def __init__(self, chapter_sign, part_sign, in_path, out_path):
-        self.chapter_sign = chapter_sign
-        self.part_sign = part_sign
+    def __init__(self, in_path, out_path, chapter_flag=True):
+        self.chapter_sign = r"^\s*(?:[上下]+[章回部节集卷话]+\s*)?[第卷话](?:\d+|[一二三四五六七八九十零〇百千两]+)[章回部节集卷话](.*)"
+        self.part_sign = r"(^\s*\d{3}.*)"
         self.in_path = in_path
         self.out_path = out_path
 
+    def get_extension(self, file_path):
+        return os.path.splitext(file_path)[1]
+
+    def get_name(self, file_path):
+        return os.path.splitext(os.path.basename(file_path))[0]
+
+    def epub_to_txt(self, epub_file):
+        txt_dir = os.path.join(self.in_path, "trans")
+        if not os.path.exists(txt_dir):
+            os.makedirs(txt_dir)
+
+        txt_path = os.path.join(txt_dir, self.get_name(epub_file) + ".txt")
+        book = epub.read_epub(epub_file)
+        with open(txt_path, "w", encoding="utf-8") as f:
+            for item in book.get_items():
+                if isinstance(item, ebooklib.epub.EpubHtml):
+                    soup = BeautifulSoup(
+                        item.get_body_content().decode("utf-8"), "html.parser"
+                    )
+                    soup = unicodedata.normalize("NFKC", soup.get_text())
+                    f.write(soup)
+        return txt_path
+
+    def mkdir(self, roll_name):
+        roll_path = os.path.join(self.out_path, roll_name)
+        if not os.path.exists(roll_path):  # 创建“卷”目录
+            try:
+                os.makedirs(roll_path)
+                print(f"文件夹 '{roll_path}' 创建成功。")
+            except OSError as e:
+                print(f"创建文件夹 '{roll_path}' 时发生错误： {e}")
+        else:
+            print(f"文件夹 '{roll_path}' 已创建")
+        return roll_path
+
     def run(self, position: dict):
         for file_name, roll_name in position.items():
-            roll_path = os.path.join(self.out_path, roll_name)
-            if not os.path.exists(roll_path):  # 创建“卷”目录
-                try:
-                    os.makedirs(roll_path)
-                    print(f"文件夹 '{roll_path}' 创建成功。")
-                except OSError as e:
-                    print(f"创建文件夹 '{roll_path}' 时发生错误： {e}")
-            else:
-                print(f"文件夹 '{roll_path}' 已创建")
-            chapters = chapter_cut(
-                self.chapter_sign, os.path.join(self.in_path, file_name)
-            )  # 划分章节
-            parts = part_cut(self.part_sign, chapters)  # 划分“部分”
+            file_path = os.path.join(self.in_path, file_name)
+            roll_path = self.mkdir(roll_name)
 
-            for chapter_name, part_dir in parts.items():
-                for part_name, part_value in part_dir.items():
-                    edge_tts_string(
-                        part_value, roll_path, f"{chapter_name}_{part_name}"
-                    )
+            if self.get_extension(file_path) == ".epub":
+                file_path = self.epub_to_txt(file_path)
+            if self.get_extension(file_path) in (".txt", ".text"):
+                chapters = chapter_cut(self.chapter_sign, file_path)  # 划分章节
+                parts = part_cut(self.part_sign, chapters)  # 划分“部分”
+                #
+                for chapter_name, part_dic in parts.items():
+                    for part_name, part_value in part_dic.items():
+                        edge_tts_string(
+                            part_value, roll_path, f"{chapter_name}_{part_name}"
+                        )
